86bba9d0be46c4276259adb065e1d040a051b401
tuliotm <tulioteodoromanso@gmail.com>
2022-12-05 10:48:03 -0300

    Adiciona autenticação básica para autenticar CRUD

--- tour-of-heros/app/controllers/heros_controller.rb
+++ tour-of-heros/app/controllers/heros_controller.rb
@@ -1,4 +1,7 @@
 class HerosController < ApplicationController
+  http_basic_authenticate_with name: Rails.application.credentials.authenticate[:name],
+                               password: Rails.application.credentials.authenticate[:password],
+                               except: %i[index show]
   before_action :set_hero, only: %i[ show edit update destroy ]
 
   # GET /heros or /heros.json
